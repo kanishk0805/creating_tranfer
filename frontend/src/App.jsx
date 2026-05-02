b34d8eef0b7515f97d94eb3bc731fbfbcff85620
kanishk0805 <kanishkgupta071@gmail.com>
2026-05-02 17:09:27 +0530

    structure created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter,Router,Route,Routes } from 'react-router-dom'
+import Signin from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
+import Send from './Pages/Send'
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/send' element={<Send />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
